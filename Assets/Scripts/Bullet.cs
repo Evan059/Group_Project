e6e45f9951e949f5a8431770ecb9ada7f455a717
Evan059 <ev522219@ucf.edu>
2025-04-05 19:01:19 -0400

    Replaced models and added clouds & music

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bullet : MonoBehaviour
+public class Bullet_Fire : MonoBehaviour
 {
     // Bullet speed
     private float bulletSpeed = 8f;
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -8,10 +9,26 @@ public class GameManager : MonoBehaviour
     public GameObject enemyBasicPrefab;
     public GameObject enemyFlowPrefab;
     public GameObject enemyAttackPrefab;
+    
+    public GameObject cloudPrefab;
+
+    public GameObject PlayerPrefab;
+
+    public TextMeshProUGUI livesText;
+
+    public float horizontalScreenSize;
+    public float verticalScreenSize;
+
+    public int score;
 
     // Start is called before the first frame update
     void Start()
     {
+        horizontalScreenSize = 10f;
+        verticalScreenSize = 6.5f;
+        score = 0;
+        Instantiate(PlayerPrefab, transform.position, Quaternion.identity);
+        CreateSky();
         InvokeRepeating("CreateEnemyBasic", 1, 2);
         InvokeRepeating("CreateEnemyFlow", 1.5f, 3f);
         InvokeRepeating("CreateEnemyAttack", 3f, 5f);
@@ -38,5 +55,22 @@ public class GameManager : MonoBehaviour
     {
         // Attack enemies spawn at the right edge of the screen and move to the left
         Instantiate(enemyAttackPrefab, new Vector3(9.5f, Random.Range(2f, 5f), 0), Quaternion.identity);
+    }
+    void CreateSky()
+    {
+        for (int i = 0; i < 30; i++)
+        {
+            Instantiate(cloudPrefab, new Vector3(Random.Range(-horizontalScreenSize, horizontalScreenSize), Random.Range(-verticalScreenSize, verticalScreenSize), 0), Quaternion.identity);
+        }
+
+    }
+    public void AddScore(int earnedScore)
+    {
+        score = score + earnedScore;
+    }
+
+    public void ChangeLivesText (int currentLives)
+    {
+        livesText.text = "Lives:" + currentLives;
     }
 }
